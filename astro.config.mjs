d1608a9ab2837eb088aa5e3b4c61a8901331502d
Jackljk <94715891+jackljk@users.noreply.github.com>
2024-11-07 15:51:31 -0800

    Added more sections and changed some names

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,23 +13,49 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: "Start Here",
+          label: "Getting Started",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Getting Started", slug: "getting_started/home" },
+            { label: "Introduction", slug: "getting_started/intro" },
+            { label: "Rules", slug: "getting_started/rules" },
+            { label: "Character Creation", slug: "getting_started/character_creation" },
+          ],
+        },
+        {
+          label: "Regions",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Aeolian Archipelago", slug: "regions/aeolian_archipelago" },
+            { label: "Lumea", slug: "regions/lumea" },
+            { label: "Karkorte", slug: "regions/karkorte" },
+            { label: "Lumea", slug: "regions/lumea" },
+            { label: "Grønn", slug: "regions/grønn" },
+            // { label: "NA", slug: "regions/NA" },
+          ],
+        },
+        {
+          label: "Species",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "species 1", slug: "species/species1" },
+            // { label: "species 2", slug: "species/species2" },
+            // { label: "species 3", slug: "species/species3" },
+            // { label: "species 4", slug: "species/species4" },
+            // { label: "species 5", slug: "species/species5" },
+          ],
+        },
+        {
+          label: "Class",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "species 1", slug: "species/species1" },
           ],
         },
         {
           label: "Character",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Introduction", slug: "character/intro" },
-            { label: "Attributes", slug: "character/attributes" },
-            { label: "Skills", slug: "character/skills" },
-            { label: "Talents", slug: "character/talents" },
-            { label: "Equipment", slug: "character/equipment" },
-            { label: "Status", slug: "character/status" },
-            { label: "Character Sheet", slug: "character/sheet" },
+            { label: "Character Creation Form", slug: "species/species1" },
           ],
         },
         {
