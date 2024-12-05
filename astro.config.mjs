c526472f7eddd76c65174c3fa7da344bd5cb6c18
Jackljk <94715891+jackljk@users.noreply.github.com>
2024-12-05 14:08:05 -0800

    fixed sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://l1joseph.github.io",
+  site: "https://tidesofchange.github.io",
   base: "/Tides-of-Change",
   integrations: [
     starlight({
